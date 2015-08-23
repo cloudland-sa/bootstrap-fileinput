102603946910c5c8008a88eb612b27c83cb471b3
alqfahad <alqfahad@gmail.com>
2015-08-23 17:24:30 +0300

    Update fileinput_locale_ar.js

--- js/fileinput_locale_ar.js
+++ js/fileinput_locale_ar.js
@@ -11,7 +11,7 @@
 (function ($) {
     "use strict";
 
-    $.fn.fileinputLocales['_LANG_'] = {
+    $.fn.fileinputLocales['ar'] = {
         fileSingle: 'ملف',
         filePlural: 'ملفات',
         browseLabel: 'استعراض &hellip;',
